788a882b170496ce493ee9d7d433a78746ff90d1
Martin Sosa <mr.martinsosa@gmail.com>
2016-01-02 17:36:12 -0500

    Added 2 commands, added replies.
    !8ball to reply, !flip to get a heads or tails, and a general reply when
    the bot is mentioned.

--- Gembot.rb
+++ Gembot.rb
@@ -8,7 +8,9 @@ bot.message(with_text: "!commands") do |event|
   event.respond "Hello! Here are all the commands I have!"
   event.respond "!ping: Pong it back!"
   event.respond "!pong: You meant ping, right?"
-  event.respond "!hi: Hi there!"  
+  event.respond "!hi: Hi there!"
+  event.respond "!flip: Flips a coin, heads or tails?"
+  event.respond "!8ball: Ask a question, see the outcome!"
 end
 
 #___________________________COMMANDS LIST______________________________________
@@ -21,9 +23,23 @@ bot.message(with_text: "!pong") do |event|
 end
 
 bot.message(with_text: "!hi") do |event|
-  event.respond "Hi there!"
+  event.respond "Hi there, #{event.user.mention}!"
+end
+
+bot.message(with_text: "!flip") do |event|
+ coin = ["heads!", "tails!"].sample
+ event.respond "#{event.user.mention} flips a coin... and it lands on #{coin}"
+end
+
+bot.message(with_text: "!8ball") do |event|
+ event.respond ["As I see it, yes", "It is certain", "It is decidedly so", "Most likely", "Outlook good", "Signs point to yes", "Without a doubt", "Yes", "Yes – definitely", "You may rely on it", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"].sample
 end
 #__________________________________END COMMANDS_______________________________
+
+#__________________________________BOT MENTION________________________________
+bot.mention do |event|
+ event.respond ["Hmmm, sounds like it might work #{event.user.mention}?" , "Seems possible, #{event.user.mention}.", "I don't get it, #{event.user.mention}.", "Have you tried something else, #{event.user.mention}?", "Try asking someone else, #{event.user.mention}",  "Might be true, #{event.user.mention}."].sample
+end
 
 
 #_______________________________RUN___________________________________________
