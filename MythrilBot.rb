fef0ca6b499cc31bd7bc5a2e386940033f603f4f
Martin <mr.martinsosa@gmail.com>
2016-06-03 22:06:54 +0000

    Added a lmgtfy search

--- MythrilBot.rb
+++ MythrilBot.rb
@@ -86,6 +86,10 @@ bot.command :urban do |event, *args|
     end
 end   
 
+bot.command :google do |event, *text|
+    "http://lmgtfy.com/?q=#{text.join('+')}"                
+end
+
 bot.message(with_text: "!8ball") do |event|
     event.respond ["As I see it, yes", "It is certain", "It is decidedly so", "Most likely", "Outlook good", "Signs point to yes", "Without a doubt", "Yes", "Yes – definitely", "You may rely on it", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"].sample
 end
